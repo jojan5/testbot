e5bd25735ec850850ebdf383b38277b8ab80879b
jojan5 <johann.elisama@gmail.com>
2024-11-06 17:53:30 -0600

    ni se que hago ayuda :[

--- index.js
+++ index.js
@@ -86,19 +86,9 @@ const client = new Client({
 })
 //-------------------------------------------------------------------------------
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 //---------------------------------------------------------------------------------------
-app.get('/', (req, res) => res.send('El bot está vivo!'));
-app.listen(process.env.PORT || 3000);
+
 
 //-----------------------------------------------------------------
 
